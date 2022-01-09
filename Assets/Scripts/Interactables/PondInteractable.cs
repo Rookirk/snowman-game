8a702988bef0ebab187bad76fc7ffb47403224bb
Herman Wu <hwu24110@gmail.com>
2022-01-09 14:48:46 -0800

    Added Pond Interactable

---
+++ Assets/Scripts/Interactables/PondInteractable.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PondInteractable : Interactable
+{
+	public Present present;
+
+	public Transform presentMidPoint;
+	public Transform presentDestination;
+
+	private float presentJumpHeight;
+	private Vector3 destinationPosition;
+
+	public float duration = 1f;
+
+	protected override void Start()
+	{
+		base.Start();
+
+		destinationPosition = presentDestination.position;
+		presentDestination.gameObject.SetActive(false);
+
+		presentJumpHeight = presentMidPoint.position.y - present.transform.position.y;
+		presentMidPoint.gameObject.SetActive(false);
+	}
+
+	public override void OnInteract()
+	{
+		DisableCollider();
+		present.transform.DOJump( destinationPosition, presentJumpHeight, 1, duration );
+		StartCoroutine( ActivatePresent() );
+	}
+
+    private IEnumerator ActivatePresent()
+    {
+        yield return new WaitForSeconds( duration );
+
+		present.Activate();
+    }
+}
