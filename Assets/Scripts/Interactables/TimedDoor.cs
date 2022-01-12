e930a94a403dbb2344c663028ae777c56dbd61d9
POLYDNK <firespert@gmail.com>
2022-01-11 22:59:11 -0800

    Added decorations and added some sfx

--- Assets/Scripts/Interactables/TimedDoor.cs
+++ Assets/Scripts/Interactables/TimedDoor.cs
@@ -18,8 +18,20 @@ public class TimedDoor : MonoBehaviour
 	private Vector3 loweredPosition;
 	private Vector3 raisedPosition;
 
+	// Audio
+	private AudioSource doorOpen;
+	private AudioSource doorClose;
+
 	private void Start()
 	{
+		// Get Audio
+		List<AudioSource> doorSounds = new List<AudioSource>();
+		GetComponents<AudioSource>(doorSounds);
+
+		doorOpen = doorSounds[0];
+		doorClose = doorSounds[1];
+
+		// Set positions
 		plate.OnPress += Raise;
 		plate.OnRelease += Lower;
 
@@ -33,12 +45,16 @@ public class TimedDoor : MonoBehaviour
 	{
 		previousPositionTween?.Kill();
 		previousPositionTween = movingDoor.DOMove( raisedPosition, timeToRaise );
+
+		doorOpen.Play(); // Play sound effect
 	}
 
 	private void Lower()
 	{
 		previousPositionTween?.Kill();
 		previousPositionTween = movingDoor.DOMove( loweredPosition, timeToLower );
+
+		doorClose.Play(); // Play sound effect
 	}
 
 	public void Lock()
