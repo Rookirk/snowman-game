36c31f09dd72c1dd1c69757b8180f2465214e282
Herman Wu <hwu24110@gmail.com>
2022-01-11 17:57:36 -0800

    Both gates open with GateInteractable

--- Assets/Scripts/Interactables/GateInteractable.cs
+++ Assets/Scripts/Interactables/GateInteractable.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 public class GateInteractable : TransformInteractable
 {
 	public ItemData key;
 	public TextMeshProUGUI selectedText;
+	public Transform Gate2;
+	public Transform Gate2Open;
+
+	private Quaternion destination2Rotation;
 
 	private AudioSource audioSource;
 
 	private void Awake()
 	{
 		audioSource = GetComponent<AudioSource>();
+	}
+
+	protected override void Start()
+	{
+		base.Start();
+
+		destination2Rotation = Gate2Open.rotation;
+		Gate2Open.gameObject.SetActive(false);
 	}
 
 	public override void Select()
@@ -37,6 +50,8 @@ public class GateInteractable : TransformInteractable
 			audioSource.Play();
 			
 			base.OnInteract();
+
+			Gate2.DORotateQuaternion( destination2Rotation, duration );
 
 			DisableCollider();
 
